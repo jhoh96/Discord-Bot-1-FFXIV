e37c1d5370cff207517e305320b253d1c8fe0eb0
unknown <justinoh1996@gmail.com>
2022-05-04 23:25:38 +0900

    initial commit

---
+++ Main/01_on_message.py
@@ -0,0 +1,26 @@
+import discord
+import os
+from dotenv import load_dotenv
+
+client = discord.Client() # connection to discord / interaction with API
+
+load_dotenv()
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+
+@client.event 
+async def on_ready(): # triggered when bot comes online
+    print('Bot is now online, listening for commands')
+
+
+@client.event
+async def on_message(message): # param - message
+    if message.author == client.user:
+        return
+
+
+    if message.content == 'hello':
+        await message.channel.send('Hi there')
+
+
+
+client.run(DISCORD_TOKEN) # do not hard code discord bot token
---
+++ Main/README.md
@@ -0,0 +1,17 @@
+# [Python] Presonal Project #1 - Discord Bot (Final Fantasy XIV)
+(Python) Presonal project using XIVAPI - for MMORPG Final Fantasy XIV.
+
+Link to XIVAPI Documentation [here](https://xivapi.com/)
+
+Link to server invitation [here]('tobeaddedlater')
+
+
+
+## Development
+
+Create a virtual environment with Python 3.7 or higher.
+
+Install required packages:
+```
+pip3 install -r requirements.txt
+```
